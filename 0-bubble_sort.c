1cbf125aecbe06cce26a1a8b66960830df0175e0
mohamedhosam <mohamedhosamopp122@icloud.com>
2024-01-24 03:49:51 -0800

    h

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,22 +8,22 @@
 void bubble_sort(int *array, size_t size)
 {
 	int swap;
-	size_t i ,k;
+	size_t i, k;
 
 	if (!array || !size)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		for (k = 0; k < size -1; k++)
+		for (k = 0; k < size - 1; k++)
 		{
-			if (array[k] > array[k+1])
+			if (array[k] > array[k + 1])
 			{
 				swap = array[k];
-				array[k] = array[k+1];
-				array[k+1] = swap;
+				array[k] = array[k + 1];
+				array[k + 1] = swap;
 				print_array(array, size);
 			}
 		}
 	}
-}   
+}
