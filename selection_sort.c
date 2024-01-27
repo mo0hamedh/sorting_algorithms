29b4e2cc428da4ba6c530d60cf4f6538f738f24d
mo0hamedh <mohamedhosamopp122@icloud.com>
2024-01-27 05:55:43 -0800

    hi

---
+++ selection_sort.c
@@ -0,0 +1,47 @@
+#include "sort.h"
+#include <stdlib.h>
+/**
+
+- swap - Function That Swapped Array Element
+- @xp: First Position
+- @yp: Second Position
+- 
+- Return: void
+*/
+void swap(int *xp, int *yp)
+{
+size_t tmp = *xp;
+*xp = *yp;
+yp = tmp;
+}
+/*
+- selection_sort - Implement Funtion That Create Selection Sort Alogrithm
+- @array: The Array To Sorted
+- @size: The Size Of An Array
+- 
+- Return: void
+*/
+void selection_sort(int *array, size_t size)
+{
+size_t i, j, minIDX;
+    
+    ```
+     for (i = 0; i < size - 1; i++)
+     {
+             minIDX = i;
+             for (j = i + 1; j < size; j++)
+             {
+                     if (array[j] < array[minIDX])
+                             minIDX = j;
+             }
+             if (minIDX != i)
+             {
+                     swap(&array[minIDX], &array[i]);
+                     print_array(array, size);
+             }
+     }
+    
+    ```
+    
+
+}
